3ef696e13700c90e755a97760654717227265c11
azoma <extremesports1307@gmail.com>
2024-06-20 19:51:33 +0500

    action 2

--- cmd/main.go
+++ cmd/main.go
@@ -25,6 +25,7 @@ func (s *server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.Cre
 		Id: gofakeit.Int64(),
 	}, nil
 }
+
 func (s *server) Get(ctx context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
 
 	return &desc.GetResponse{
